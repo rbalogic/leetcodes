913a026f3a262a31dd560930dea7b0233623f54b
Balaji R <rbalajives@gmail.com>
2026-06-17 23:41:50 +0530

    feat: fix singleNumber problem

--- singleNumber.js
+++ singleNumber.js
@@ -4,15 +4,24 @@
  */
 var singleNumber = function (nums) {
   let numbers = nums;
+  let uniqueElements = [];
+  let duplicateElements = [];
   if (numbers.length === 1) {
     return numbers[0];
   }
-  for (i = 0; i < numbers.length; i++) {
-    if (numbers.includes(numbers[i])) {
-      numbers.splice(i, 1);
+  numbers.forEach((item) => {
+    if (uniqueElements.length === 0) {
+      uniqueElements.push(item);
+    } else if (uniqueElements.includes(item)) {
+      duplicateElements.push(item);
+    } else {
+      uniqueElements.push(item);
     }
-  }
-  console.log(numbers);
+  });
+  const finalElement = uniqueElements.filter(
+    (e) => !duplicateElements.includes(e),
+  );
+  return finalElement[0];
 };
 
 singleNumber([3, 3, 5, 5, 1]);
