bfe606958d5c000a0752c29e1a7a37e30af04a28
Balaji R <rbalajives@gmail.com>
2026-07-28 01:43:02 +0530

    feat: runningSum lcprob

---
+++ runningSum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] = nums[i] + nums[i - 1];
+  }
+  return nums;
+};
+
+console.log(runningSum([1, 2, 3, 4]));
