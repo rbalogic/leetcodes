653c0fddacd2978da374900c73339bb322750da0
Balaji R <rbalajives@gmail.com>
2026-07-30 03:23:15 +0530

    feat: lcprob 2951

---
+++ findPeaks.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} mountain
+ * @return {number[]}
+ */
+var findPeaks = function (mountain) {
+  let peaks = [];
+  for (let i = 1; i < mountain.length - 1; i++) {
+    if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
+      peaks.push(i);
+    }
+  }
+  return peaks;
+};
+
+console.log(findPeaks([1, 4, 3, 8, 5]));
